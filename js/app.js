f654a378da5e062d724d7cb888a971e05640e7d4
renepk <meistromaster@earthlink.net>
2014-06-19 16:03:39 -0400

    haduken project

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '900px'},
+      700,
+      function() {
+        $(this).hide();
+        $(this).css('left', '455px');
+      }
+    );
+
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+$(this).keydown(function(e){
+      if (e.keyCode == 88) { 
+    $('.ryu-ready').hide();
+    $('.ryu-cool').show();
+  }}
+);
+$(this).keyup(function(e){
+      if (e.keyCode == 88) { 
+    $('.ryu-cool').hide();
+    $('.ryu-ready').show();
+  }}
+
+);
+
+
+});
+  
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
